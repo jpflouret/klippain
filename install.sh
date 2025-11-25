9940175fd9992a5fb497d84d8c250c8ac29b2c1a
JP Flouret <jpflouret@gmail.com>
2025-11-24 23:24:45 -0800

    Don't install klippain-shaketune

--- install.sh
+++ install.sh
@@ -263,7 +263,7 @@ backup_config
 install_config
 restart_klipper
 
-wget -O - https://raw.githubusercontent.com/jpflouret/klippain-shaketune/main/install.sh | bash
+# wget -O - https://raw.githubusercontent.com/jpflouret/klippain-shaketune/main/install.sh | bash
 
 echo "[POST-INSTALL] Everything is ok, Klippain installed and up to date!"
 echo "[POST-INSTALL] Be sure to check the breaking changes on the release page: https://github.com/Frix-x/klippain/releases"
